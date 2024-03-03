19779fd95436de2ef595e3e43c51d75c60f239d2
Carsten Engelke <carsten.engelke@gmail.com>
2024-03-03 11:28:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ function updateCards(list) {
             container.innerHTML = "<h4><b>" + obj.title + " <p style=\"color:Gray;\">(" + obj.title_EN + ")</p></b></h4>"+
             "<h5>" + obj.type + "</h5><p>" + obj.text + "</p>";
             card.appendChild(container);
+        container.addEventListener("click","copyToClipboard()")
         card.addEventListener("click","copyToClipboard()")
         document.getElementById("result").appendChild(card);
     }
