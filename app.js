1f82e164d64d6efc2e1fc0e4468c2d0ab5acff7a
Carsten Engelke <carsten.engelke@gmail.com>
2024-03-03 13:11:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ function updateCards(list) {
             "<h5>" + obj.type + "</h5><p>" + obj.text + "</p>";
         card.appendChild(container);
         document.getElementById("result").appendChild(card);
-        card.addEventListener("click",function() {alert("moin")});
+        card.addEventListener("click", () => {alert("moin")});
     }
 }
 
